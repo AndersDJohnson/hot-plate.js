417410077abc7a05db7d9020100f4016fa1d3ae2
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-05-23 19:50:12 -0500

    add time and size controls

--- src/js/browser.js
+++ src/js/browser.js
@@ -1,9 +1,12 @@
 var core = require('./core');
 var browserCore = require('./browser-core');
 var $ = require('jquery');
+var _ = require('lodash');
 
 var $turns = $('#turns');
 var $diff = $('#diff');
+var $size = $('#size');
+var $time = $('#time');
 
 var instance;
 
@@ -11,10 +14,15 @@ var run = function () {
   if (instance) {
     instance.stop();
   }
+
+  var sizes = _.map($size.val().split('x'), function (x) {
+    return parseInt(x);
+  });
+
   instance = browserCore({
-    plate: new core.Plate(16, 16),
+    plate: new core.Plate(sizes[0], sizes[1]),
     gridContainer: $('#grid-container'),
-    turnTime: 50,
+    turnTime: parseInt($time.val()),
     onTurn: function (state, cb) {
       $turns.text(state.turns);
       $diff.text(state.diffMax);
